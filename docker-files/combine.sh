d450e9a9e058cbe5162420672597d28a120e91f5
Xieyt <alokmilenium@gmail.com>
2023-08-02 19:20:25 +0530

    fix: manifests not creating properly

--- docker-files/combine.sh
+++ docker-files/combine.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -rf ~/.docker/manifests
 docker manifest create xieytx/fm-nginx:latest \
 --amend xieytx/fm-nginx:amd64 \
 --amend xieytx/fm-nginx:arm64
@@ -16,8 +17,6 @@ docker manifest create xieytx/fm-rqdashboard:latest \
 --amend xieytx/fm-rqdashboard:arm64
 
 docker manifest push xieytx/fm-rqdashboard:latest
-
-docker pull xieytx/fm-frappe:amd64
 
 docker manifest create xieytx/fm-frappe:latest \
 --amend xieytx/fm-frappe:amd64 \
