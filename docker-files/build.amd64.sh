be16157d5072de8d9dcfa7420b03345ee73cb5fc
Xieyt <alokmilenium@gmail.com>
2023-09-07 18:40:19 +0530

    remove: rq-dashboard service

--- docker-files/build.amd64.sh
+++ docker-files/build.amd64.sh
@@ -2,5 +2,4 @@
 
 docker build --push --platform linux/amd64 -t xieytx/fm-nginx:amd64 docker-images/nginx-docker/.
 docker build --push --platform linux/amd64 -t xieytx/fm-mailhog:amd64 docker-images/mailhog-docker/.
-docker build --push --platform linux/amd64 -t xieytx/fm-rqdashboard:amd64 docker-images/rq-dashboard-docker/.
 docker build --push --platform linux/amd64 -t xieytx/fm-frappe:amd64 docker-images/frappe-docker/.
