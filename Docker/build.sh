6149c989cb33913fe4d273cfd11f0254ca91a020
Xieyt <alokmilenium@gmail.com>
2023-10-13 17:02:19 +0530

    fix: docker build script

--- Docker/build.sh
+++ Docker/build.sh
@@ -3,7 +3,7 @@ TAG='v0.8.0'
 ARCH=$(uname -m)
 
 # arm64
-if [[ "${ARCH}" -eq 'arm64' ]]; then
+if [[ "${ARCH}" == 'arm64' ]]; then
     echo "Building arm64 Images with tag arm64-${TAG}"
     docker build --push --platform linux/arm64 -t xieytx/fm-nginx:arm64-"${TAG}" nginx/.
     docker build --push --platform linux/arm64 -t xieytx/fm-mailhog:arm64-"${TAG}" mailhog/.
@@ -11,30 +11,30 @@ if [[ "${ARCH}" -eq 'arm64' ]]; then
 fi
 
 #amd64
-if [[ "${ARCH}" -eq 'x86_64' ]]; then
+if [[ "${ARCH}" == 'x86_64' ]]; then
     echo "Building arm64 Images with tag amd64-${TAG}"
     docker build --push --platform linux/amd64 -t xieytx/fm-nginx:amd64-"${TAG}" nginx/.
     docker build --push --platform linux/amd64 -t xieytx/fm-mailhog:amd64-"${TAG}" mailhog/.
     docker build --push --platform linux/amd64 -t xieytx/fm-frappe:amd64-"${TAG}" frappe/.
 
     echo "Combining arm64 and amd64 tags.."
-rm -rf ~/.docker/manifests
+    rm -rf ~/.docker/manifests
 
-docker manifest create xieytx/fm-nginx:"$TAG" \
---amend xieytx/fm-nginx:amd64-"$TAG" \
---amend xieytx/fm-nginx:arm64-"$TAG"
+    docker manifest create xieytx/fm-nginx:"$TAG" \
+    --amend xieytx/fm-nginx:amd64-"$TAG" \
+    --amend xieytx/fm-nginx:arm64-"$TAG"
 
-docker manifest push xieytx/fm-nginx:"$TAG"
+    docker manifest push xieytx/fm-nginx:"$TAG"
 
-docker manifest create xieytx/fm-mailhog:"$TAG" \
---amend xieytx/fm-mailhog:amd64-"$TAG" \
---amend xieytx/fm-mailhog:arm64-"$TAG"
+    docker manifest create xieytx/fm-mailhog:"$TAG" \
+    --amend xieytx/fm-mailhog:amd64-"$TAG" \
+    --amend xieytx/fm-mailhog:arm64-"$TAG"
 
-docker manifest push xieytx/fm-mailhog:"$TAG"
+    docker manifest push xieytx/fm-mailhog:"$TAG"
 
-docker manifest create xieytx/fm-frappe:"$TAG" \
---amend xieytx/fm-frappe:amd64-"$TAG" \
---amend xieytx/fm-frappe:arm64-"$TAG"
+    docker manifest create xieytx/fm-frappe:"$TAG" \
+    --amend xieytx/fm-frappe:amd64-"$TAG" \
+    --amend xieytx/fm-frappe:arm64-"$TAG"
 
-docker manifest push xieytx/fm-frappe:"$TAG"
+    docker manifest push xieytx/fm-frappe:"$TAG"
 fi
