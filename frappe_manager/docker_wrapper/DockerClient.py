ceb4c480fee7a3d26230d08c597f2db4deb2404a
Xieyt <alokmilenium@gmail.com>
2024-01-03 15:37:27 +0530

    add docker cp,rm,kill and run commands

--- frappe_manager/docker_wrapper/DockerClient.py
+++ frappe_manager/docker_wrapper/DockerClient.py
@@ -1,5 +1,8 @@
 from typing import Literal, Optional
 import json
+
+import rich
+from frappe_manager.docker_wrapper.DockerException import DockerException
 from frappe_manager.docker_wrapper.DockerCompose import DockerComposeWrapper
 from pathlib import Path
 from frappe_manager.docker_wrapper.utils import (
@@ -36,9 +39,116 @@ class DockerClient:
             for source ,line in iterator:
                 if source == 'stdout':
                     output = json.loads(line.decode())
-        except Exception:
+        except DockerException as e:
             return {}
         return output
+
+
+    def cp(
+        self,
+        source: str,
+        destination: str,
+        source_container: str = None,
+        destination_container: str = None,
+        archive: bool = False,
+        follow_link: bool = False,
+        quiet: bool = False,
+        stream: bool = False,
+        stream_only_exit_code: bool = False,
+    ):
+        parameters: dict = locals()
+        cp_cmd: list = ["cp"]
+
+        remove_parameters = ["stream", "stream_only_exit_code", "source", "destination","source_container","destination_container"]
+
+        cp_cmd += parameters_to_options(parameters, exclude=remove_parameters)
+
+        if source_container:
+            source = f"{source_container}:{source}"
+
+        if destination_container:
+            destination = f"{destination_container}:{destination}"
+
+        cp_cmd += [f'{source}']
+        cp_cmd += [f'{destination}']
+
+        iterator = run_command_with_exit_code(
+            self.docker_cmd + cp_cmd, quiet=stream_only_exit_code,
+            stream=stream
+        )
+        return iterator
+
+    def kill(
+        self,
+        container: str,
+        signal: Optional[str] = None,
+        stream: bool = False,
+        stream_only_exit_code: bool = False,
+    ):
+        parameters: dict = locals()
+        kill_cmd: list = ["kill"]
+
+        remove_parameters = ["stream", "stream_only_exit_code", "container"]
+
+        kill_cmd += parameters_to_options(parameters, exclude=remove_parameters)
+        kill_cmd += [f'{container}']
+
+        iterator = run_command_with_exit_code(
+            self.docker_cmd + kill_cmd, quiet=stream_only_exit_code,
+            stream=stream
+        )
+        return iterator
+
+    def rm(
+        self,
+        container:str,
+        force: bool = False,
+        link: bool = False,
+        volumes: bool = False,
+        stream: bool = False,
+        stream_only_exit_code: bool = False,
+    ):
+        parameters: dict = locals()
+        rm_cmd: list = ["rm"]
+
+        remove_parameters = ["stream", "stream_only_exit_code","container"]
+
+        rm_cmd += parameters_to_options(parameters, exclude=remove_parameters)
+        rm_cmd += [f'{container}']
+
+        iterator = run_command_with_exit_code(
+            self.docker_cmd + rm_cmd, quiet=stream_only_exit_code,
+            stream=stream
+        )
+        return iterator
+
+    def run(
+        self,
+        command: str,
+        image: str,
+        name: Optional[str] = None,
+        detach: bool = False,
+        entrypoint: Optional[str] = None,
+        stream: bool = False,
+        stream_only_exit_code: bool = False,
+    ):
+        parameters: dict = locals()
+        run_cmd: list = ["run"]
+
+        remove_parameters = ["stream", "stream_only_exit_code","image","command"]
+
+        run_cmd += parameters_to_options(parameters, exclude=remove_parameters)
+        run_cmd += [f'{image}']
+
+        if command:
+            run_cmd += [f'{command}']
+
+        iterator = run_command_with_exit_code(
+            self.docker_cmd + run_cmd, quiet=stream_only_exit_code,
+            stream=stream
+        )
+        return iterator
+
 
     def server_running(self) -> bool:
         """
