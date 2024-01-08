3494c0921a879aa37cd0bfe6aff2e78d097b3512
Xieyt <alokmilenium@gmail.com>
2024-01-08 16:09:54 +0530

    add func get_container_hex in utils

--- frappe_manager/site_manager/utils.py
+++ frappe_manager/site_manager/utils.py
@@ -49,25 +49,23 @@ def check_ports(ports):
         return already_binded
 
 def log_file(file, refresh_time:float = 0.1, follow:bool =False):
-    '''generator function that yields new lines in a file
     '''
-    # while True:
-    #     line = file.readline()        # sleep if file hasn't been updated
-    #     yield line
-    #     if not line:
-    #         break
-    # seek the end of the file
-    # file.seek(0, os.SEEK_END)
+    Generator function that yields new lines in a file
+    '''
     file.seek(0)
 
     # start infinite loop
     while True:
         # read last line of file
-        line = file.readline()        # sleep if file hasn't been updated
+        line = file.readline()
         if not line:
             if not follow:
                 break
+            # sleep if file hasn't been updated
             time.sleep(refresh_time)
             continue
         line = line.strip('\n')
         yield line
+
+def  get_container_name_prefix(site_name):
+        return site_name.replace('.','')
