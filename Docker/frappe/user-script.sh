e48ab9da01356ca12d4648199ad6a06232ba23bd
Xieyt <alokmilenium@gmail.com>
2024-01-09 22:47:10 +0530

    symlink for bench-start.log

--- Docker/frappe/user-script.sh
+++ Docker/frappe/user-script.sh
@@ -9,7 +9,7 @@ emer() {
 
 if [[ ! -d 'logs'  ]]; then
     mkdir -p logs
-    ln -sfn ../frappe-bench/logs/web.dev.log logs/web.dev.log
+    ln -sfn ../frappe-bench/logs/web.dev.log logs/bench-start.log
 fi
 
 REDIS_SOCKETIO_PORT=80
