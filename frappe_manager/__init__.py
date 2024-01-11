34aa6e47c3493d6e67be1890ad85f5e8df721367
Xieyt <alokmilenium@gmail.com>
2024-01-11 17:26:41 +0530

    fix: typo

--- frappe_manager/__init__.py
+++ frappe_manager/__init__.py
@@ -24,5 +24,5 @@ class SiteServicesEnum(str, Enum):
     redis_queue = "redis-queue"
     redis_cache = "redis-cache"
     redis_socketio = "redis-socketio"
-    scheduler = "scheduler"
+    schedule = "schedule"
     socketio = "socketio"
