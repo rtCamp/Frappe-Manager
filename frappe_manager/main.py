2b7a64d574125315454ba97f9f275cc16579a866
Xieyt <alokmilenium@gmail.com>
2024-02-27 04:22:38 +0530

    enhance exception logging

--- frappe_manager/main.py
+++ frappe_manager/main.py
@@ -1,7 +1,7 @@
 import atexit
 from frappe_manager.display_manager.DisplayManager import richprint
 from frappe_manager.logger import log
-from frappe_manager.utils.helpers import check_update,  remove_zombie_subprocess_process
+from frappe_manager.utils.helpers import check_update, remove_zombie_subprocess_process
 from frappe_manager.utils.docker import process_opened
 from frappe_manager.commands import app
 
@@ -11,8 +11,12 @@ def cli_entrypoint():
         app()
     except Exception as e:
         logger = log.get_logger()
-        logger.exception(f"Exception:  : {e}")
-        raise e
+        richprint.stop()
+        with richprint.stdout.capture() as capture:
+            richprint.stdout.print_exception(show_locals=True)
+        excep = capture.get()
+        logger.error(f"Exception Occured:  : \n{excep}")
+
     finally:
         atexit.register(exit_cleanup)
 
@@ -21,5 +25,5 @@ def exit_cleanup():
     This function is used to perform cleanup at the exit.
     """
     remove_zombie_subprocess_process(process_opened)
-    #check_update()
+    check_update()
     richprint.stop()
