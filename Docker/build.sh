d7d152757ee43c52ab6207c1e23fd8901218da6d
Xieyt <alokmilenium@gmail.com>
2024-03-19 15:43:23 +0530

    update image build script

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,40 +1,47 @@
 #!/usr/bin/env bash
-TAG='v0.10.0'
+set -x
 ARCH=$(uname -m)
 
-# arm64
-if [[ "${ARCH}" == 'arm64' ]]; then
-    echo "Building arm64 Images with tag arm64-${TAG}"
-    docker build --push --platform linux/arm64 -t ghcr.io/rtcamp/frappe-manager-nginx:arm64-"${TAG}" nginx/.
-    docker build --push --platform linux/arm64 -t ghcr.io/rtcamp/frappe-manager-mailhog:arm64-"${TAG}" mailhog/.
-    docker build --push --platform linux/arm64 -t ghcr.io/rtcamp/frappe-manager-frappe:arm64-"${TAG}" frappe/.
+IMAGE_NAME_PREFIX="ghcr.io/rtcamp/frappe-manager"
+COMMAND='docker build --push'
+
+OTHER_ARCH="x86_64"
+if [[ "${ARCH}" == "x86_64" ]]; then
+    ARCH="amd64"
+    OTHER_ARCH="arm64"
 fi
 
-#amd64
-if [[ "${ARCH}" == 'x86_64' ]]; then
-    echo "Building amd64 Images with tag amd64-${TAG}"
-    docker build --push --platform linux/amd64 -t ghcr.io/rtcamp/frappe-manager-nginx:amd64-"${TAG}" nginx/.
-    docker build --push --platform linux/amd64 -t ghcr.io/rtcamp/frappe-manager-mailhog:amd64-"${TAG}" mailhog/.
-    docker build --push --platform linux/amd64 -t ghcr.io/rtcamp/frappe-manager-frappe:amd64-"${TAG}" frappe/.
+# images=$(jq -rc '. | keys[] ' images-tag.json || exit 0)
 
-    echo "Combining arm64 and amd64 tags.."
-    rm -rf ~/.docker/manifests
+images='frappe'
 
-    docker manifest create ghcr.io/rtcamp/frappe-manager-nginx:"$TAG" \
-    --amend ghcr.io/rtcamp/frappe-manager-nginx:amd64-"$TAG" \
-    --amend ghcr.io/rtcamp/frappe-manager-nginx:arm64-"$TAG"
+for image in ${images}; do
+    IMAGE_TAG=$(jq -rc ".${image}" images-tag.json || exit 0)
+    if [[ "${IMAGE_TAG:-}" ]]; then
 
-    docker manifest push ghcr.io/rtcamp/frappe-manager-nginx:"$TAG"
+        if [[ "${ARCH}" == 'arm64' ]]; then
+            COMMAND+=" --provenance false"
+        fi
 
-    docker manifest create ghcr.io/rtcamp/frappe-manager-mailhog:"$TAG" \
-    --amend ghcr.io/rtcamp/frappe-manager-mailhog:amd64-"$TAG" \
-    --amend ghcr.io/rtcamp/frappe-manager-mailhog:arm64-"$TAG"
+        IMAGE_NAME="${IMAGE_NAME_PREFIX}-${image}"
+        IMAGE_NAME_WITH_TAG="${IMAGE_NAME}:${ARCH}-${IMAGE_TAG}"
+        CONTEXT_DIR="${image}/."
 
-    docker manifest push ghcr.io/rtcamp/frappe-manager-mailhog:"$TAG"
+        echo "Building ${IMAGE_NAME_WITH_TAG}"
 
-    docker manifest create ghcr.io/rtcamp/frappe-manager-frappe:"$TAG" \
-    --amend ghcr.io/rtcamp/frappe-manager-frappe:amd64-"$TAG" \
-    --amend ghcr.io/rtcamp/frappe-manager-frappe:arm64-"$TAG"
+        COMMAND+=" --platform linux/${ARCH} -t ${IMAGE_NAME_WITH_TAG} $CONTEXT_DIR"
 
-    docker manifest push ghcr.io/rtcamp/frappe-manager-frappe:"$TAG"
-fi
+        eval "${COMMAND}"
+        STATUS="$?"
+
+
+        if [[ "${STATUS}" -eq 0 ]]; then
+            echo "Combining"
+            rm -rf ~/.docker/manifests
+            docker manifest create "${IMAGE_NAME}:${IMAGE_TAG}" \
+            --amend "${IMAGE_NAME_WITH_TAG}" \
+            --amend "${IMAGE_NAME}:${OTHER_ARCH}-${IMAGE_TAG}"
+        fi
+
+    fi
+done
