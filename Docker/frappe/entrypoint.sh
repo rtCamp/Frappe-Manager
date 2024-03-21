6f09574e9e8dde7b99a2cd7f431d19f68b4cc3bb
Xieyt <alokmilenium@gmail.com>
2024-03-21 19:55:12 +0530

    fix: frappe container some files permission issue

--- Docker/frappe/entrypoint.sh
+++ Docker/frappe/entrypoint.sh
@@ -19,21 +19,19 @@ mkdir -p /opt/user/conf.d
 chown -R "$USERID":"$USERGROUP" /opt
 
 if [[ ! -d "/workspace/.oh-my-zsh" ]]; then
-   cp -r /opt/user/.oh-my-zsh /workspace/.oh-my-zsh
+   cp -pr /opt/user/.oh-my-zsh /workspace/
 fi
 
 if [[ ! -f "/workspace/.zshrc" ]]; then
-   cat /opt/user/.zshrc > /workspace/.zshrc
+   cp -p /opt/user/.zshrc  /workspace/
 fi
 
 if [[ ! -f "/workspace/.profile" ]]; then
-   cat /opt/user/.profile > /workspace/.profile
+   cp -p /opt/user/.profile  /workspace/
 fi
 
 chown "$USERID":"$USERGROUP" /workspace /workspace/frappe-bench
-
-ls -p /workspace | grep -v 'frappe-bench/' | xargs -I{} chown -R "$USERID":"$USERGROUP" /workspace{}
-
+ls -pA /workspace | xargs -I{} chown -R "$USERID":"$USERGROUP" /workspace/{} &
 
 if [ "$#" -gt 0 ]; then
     gosu "$USERID":"$USERGROUP" "/scripts/$@"
