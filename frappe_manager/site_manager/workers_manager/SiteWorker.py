3477c6ed0ada3506aa114428c05d2710a4829720
Alok Singh <alokmilenium@gmail.com>
2025-01-29 06:58:50 +0530

    refactor: Simplify environment config in docker templates
    Signed-off-by: Alok Singh <alokmilenium@gmail.com>

--- frappe_manager/site_manager/workers_manager/SiteWorker.py
+++ frappe_manager/site_manager/workers_manager/SiteWorker.py
@@ -87,9 +87,6 @@ class BenchWorkers:
             worker_config = deepcopy(template_worker_config)
 
             # setting environments
-            worker_config["environment"]["SUPERVISOR_SERVICE_CONFIG_FILE_NAME"] = str(
-                worker_config["environment"]["SUPERVISOR_SERVICE_CONFIG_FILE_NAME"]
-            ).replace("{worker-name}", worker)
             worker_config["environment"]["USERID"] = os.getuid()
             worker_config["environment"]["USERGROUP"] = os.getgid()
             worker_config["environment"]["SERVICE_NAME"] = worker
@@ -97,12 +94,8 @@ class BenchWorkers:
             self.compose_project.compose_file_manager.yml["services"][worker] = worker_config
 
         self.compose_project.compose_file_manager.set_container_names(get_container_name_prefix(self.bench.name))
-
         self.compose_project.compose_file_manager.set_version(get_current_fm_version())
         self.compose_project.compose_file_manager.set_root_networks_name(
             'site-network', get_container_name_prefix(self.bench.name)
         )
-        # self.compose_project.compose_file_manager.yml["networks"]["site-network"]["name"] = (
-        #     get_container_name_prefix(self.bench.name) + f"-network"
-        # )
         self.compose_project.compose_file_manager.write_to_file()
