43c64ec8026e400901d3d95ad867e69bf987b188
Alok Singh <alokmilenium@gmail.com>
2025-04-04 20:50:26 +0530

    ci: Add GitHub Actions workflow to build Linux binaries
    Signed-off-by: Alok Singh <alokmilenium@gmail.com>

---
+++ .github/scripts/get_version.py
@@ -0,0 +1,16 @@
+import os
+
+env_file = os.getenv("GITHUB_ENV")
+
+with open("pyproject.toml") as f:
+    for line in f.readlines():
+        if "version" in line:
+            version = line.split("=")[1]
+            version = version.strip()
+            version = version.strip('"')
+
+            with open(env_file, "a") as myfile:
+                myfile.write("PYAPP_PROJECT_VERSION=" + version + "\n")
+                path = os.path.abspath(f"./dist/frappe_manager-{version}.tar.gz")
+                myfile.write(f"PYAPP_PROJECT_PATH={path}\n")
+            exit(0)
