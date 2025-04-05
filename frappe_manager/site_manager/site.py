18f1a559f3e591eb07976ae30609d0adeea6ec5b
Alok Singh <alokmilenium@gmail.com>
2025-04-05 06:22:13 +0530

    fix: Log exception details during certificate removal
    Signed-off-by: Alok Singh <alokmilenium@gmail.com>

--- frappe_manager/site_manager/site.py
+++ frappe_manager/site_manager/site.py
@@ -1121,7 +1121,7 @@ class Bench:
         try:
             self.remove_certificate()
         except Exception as e:
-            # self.logger.exception(e)
+            self.logger.exception(e)
             richprint.warning(str(e))
 
         self.remove_database_and_user()
